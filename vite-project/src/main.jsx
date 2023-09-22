f6e6874e0443c0cfd10a3d6dfb85b76445c01724
ARVA <139310320+ArvaDev@users.noreply.github.com>
2023-09-21 21:50:20 -0600

    Update main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './Pages/Home'
 
+//Armando Rodríguez Villarreal
+//Jose Norberto Lopez Gomez
+//Isai Abel Lopez Sanchez
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     < Home/>
